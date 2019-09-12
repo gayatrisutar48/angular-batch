3e114c572ae374d6c2ec2afb54decbd47f8d04c7
gayatrisutar48 <g.gsutar48@gmail.com>
2019-09-12 23:56:27 +0530

    comp-inline basics

--- angular/intro-apporproject/src/app/app.module.ts
+++ angular/intro-apporproject/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FirstComponent } from './first/first.component';
+import { MyAlComponent } from './first/my-al.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponent,
+    MyAlComponent
   ],
   imports: [
     BrowserModule
---
+++ angular/intro-apporproject/src/app/first/my-al.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-al',
+  template: `
+  <div class="alert alert-info mt-3">
+  ii)Property binding(it works with anything i.e, object,string..etc it written in ) iii)Event Binding iv)one way binding 
+  </div>
+  
+  `,
+  styles: []
+})
+export class MyAlComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
